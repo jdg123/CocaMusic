3b606d1a1141a9f92726b80bb4376e1a759f0781
Josh Greenberger <jdgreenb@usc.edu>
2014-11-03 20:48:01 -0800

    Made melody pentatonic. Need to fix melody overlap on midi

--- Coca_Music/src/Music.java
+++ Coca_Music/src/Music.java
@@ -13,19 +13,18 @@ public final class Music implements JMC {
 
 		Score s = new Score("JMDemo1 - Scale");
 		Part p = new Part("Flute", PIANO, 0);
-		Phrase melody = generateMelody("EMILY LIU", 60);
-
-		// Read.midi(s, "Embarrassed.mid");
-		// Play.midi(s);
-		// return;
+		
+		//Read.midi(s, "Embarrassed.mid");
+		Phrase melody = generateMelody("NOEL BUTTA", 74);	
 
 		// Phrase phr = new Phrase("Chromatic Scale", 0.0);
 		// for(int i=0;i<12;i++){
 		// Note n = new Note(C4+i, THIRTYSECOND_NOTE);
 		// phr.addNote(n);
 		// }
+		//p.addPhrase(phr);
 
-		// p.addPhrase(phr);
+		
 		p.addPhrase(melody);
 		s.addPart(p);
 		Play.midi(s);
@@ -50,49 +49,49 @@ public final class Music implements JMC {
 				n = new Note(key + 2, CROTCHET);
 				break;
 			case 'd':
-				n = new Note(key + 3, CROTCHET);
-				break;
-			case 'e':
 				n = new Note(key + 4, CROTCHET);
 				break;
-			case 'f':
+			case 'e':
 				n = new Note(key + 5, CROTCHET);
 				break;
+			case 'f':
+				n = new Note(key, CROTCHET);
+				break;
 			case 'g':
-				n = new Note(key + 6, CROTCHET);
+				n = new Note(key + 1, CROTCHET);
 				break;
 			case 'h':
-				n = new Note(key, EIGHTH_NOTE);
-				break;
-			case 'i':
-				n = new Note(key + 1, EIGHTH_NOTE);
-				break;
-			case 'j':
 				n = new Note(key + 2, EIGHTH_NOTE);
 				break;
-			case 'k':
-				n = new Note(key + 3, EIGHTH_NOTE);
-				break;
-			case 'l':
+			case 'i':
 				n = new Note(key + 4, EIGHTH_NOTE);
 				break;
-			case 'm':
+			case 'j':
 				n = new Note(key + 5, EIGHTH_NOTE);
 				break;
+			case 'k':
+				n = new Note(key, EIGHTH_NOTE);
+				break;
+			case 'l':
+				n = new Note(key + 1, EIGHTH_NOTE);
+				break;
+			case 'm':
+				n = new Note(key + 2, EIGHTH_NOTE);
+				break;
 			case 'n':
-				n = new Note(key + 6, EIGHTH_NOTE);
+				n = new Note(key + 4, EIGHTH_NOTE);
 				break;
 			case 'o':
-				n = new Note(key, SIXTEENTH_NOTE);
+				n = new Note(key + 5, SIXTEENTH_NOTE);
 				break;
 			case 'p':
-				n = new Note(key + 1, SIXTEENTH_NOTE);
+				n = new Note(key, SIXTEENTH_NOTE);
 				break;
 			case 'q':
-				n = new Note(key + 2, SIXTEENTH_NOTE);
+				n = new Note(key + 1, SIXTEENTH_NOTE);
 				break;
 			case 'r':
-				n = new Note(key + 3, SIXTEENTH_NOTE);
+				n = new Note(key + 2, SIXTEENTH_NOTE);
 				break;
 			case 's':
 				n = new Note(key + 4, SIXTEENTH_NOTE);
@@ -101,28 +100,22 @@ public final class Music implements JMC {
 				n = new Note(key + 5, SIXTEENTH_NOTE);
 				break;
 			case 'u':
-				n = new Note(key + 6, SIXTEENTH_NOTE);
+				n = new Note(key, SIXTEENTH_NOTE);
 				break;
 			case 'v':
-				n = new Note(key, EIGHTH_NOTE_TRIPLET);
-				break;
-			case 'w':
 				n = new Note(key + 1, EIGHTH_NOTE_TRIPLET);
 				break;
-			case 'x':
+			case 'w':
 				n = new Note(key + 2, EIGHTH_NOTE_TRIPLET);
 				break;
-			case 'y':
-				n = new Note(key + 3, EIGHTH_NOTE_TRIPLET);
-				break;
-			case 'z':
+			case 'x':
 				n = new Note(key + 4, EIGHTH_NOTE_TRIPLET);
 				break;
-			case ' ':
+			case 'y':
 				n = new Note(key + 5, EIGHTH_NOTE_TRIPLET);
 				break;
 			default:
-				n = new Note(key + 6, EIGHTH_NOTE_TRIPLET);
+				n = new Note(key + 5, EIGHTH_NOTE_TRIPLET);
 			}
 			System.out.println(n);
 			melody.addNote(n);
