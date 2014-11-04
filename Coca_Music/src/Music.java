b53ffd7abc8a6d671fdef2bd9f2b6d5a7c0b1bc0
Josh Greenberger <jdgreenb@usc.edu>
2014-11-03 20:37:38 -0800

    Wrote melody by name function. Sounds bad right now.

--- Coca_Music/src/Music.java
+++ Coca_Music/src/Music.java
@@ -5,63 +5,129 @@ import javax.sound.midi.Sequencer;
 
 import jm.JMC;
 import jm.music.data.*;
-import jm.util.*; 
+import jm.util.*;
 
+public final class Music implements JMC {
 
-public final class Music implements JMC{
-	
-	public static void main(String[] args){
-		
+	public static void main(String[] args) {
+
 		Score s = new Score("JMDemo1 - Scale");
-		Part p = new Part("Flute", BELLS, 0);
-		Phrase melody = generateMelody("abge", C4);
-//		Read.midi(s, "Exhausted.mid");
-//		Play.midi(s);
-//		return;
-		
-//		Phrase phr = new Phrase("Chromatic Scale", 0.0);
-//		for(int i=0;i<12;i++){
-//			Note n = new Note(C4+i, THIRTYSECOND_NOTE);
-//			phr.addNote(n);
-//		}
-		
+		Part p = new Part("Flute", PIANO, 0);
+		Phrase melody = generateMelody("EMILY LIU", 60);
+
+		// Read.midi(s, "Embarrassed.mid");
+		// Play.midi(s);
+		// return;
+
+		// Phrase phr = new Phrase("Chromatic Scale", 0.0);
+		// for(int i=0;i<12;i++){
+		// Note n = new Note(C4+i, THIRTYSECOND_NOTE);
+		// phr.addNote(n);
+		// }
+
+		// p.addPhrase(phr);
 		p.addPhrase(melody);
 		s.addPart(p);
-		//Read.midi(s, "test.mid");
-		//View.show(s);
 		Play.midi(s);
 	}
-	
-	public static Phrase generateMelody (String name, double key){
+
+	public static Phrase generateMelody(String name, int key) {
 		Phrase melody = new Phrase("Melody", 0.0);
 		name = name.toLowerCase();
-		//Iterate through letters in name
-		 
-		for (int i = 0; i < name.length(); i++){
+		// Iterate through letters in name
+		for (int x = 0; x < 4; x++){
+		for (int i = 0; i < name.length(); i++) {
 			Note n = null;
-			//Note on first beat
-			if (name.charAt(i) == 'a') 
+			// Note on first beat
+			switch (name.charAt(i)) {
+			case 'a':
 				n = new Note(key, CROTCHET);
-			else if (name.charAt(i) == 'b') 
+				break;
+			case 'b':
 				n = new Note(key + 1, CROTCHET);
-			else if (name.charAt(i) == 'c') 
+				break;
+			case 'c':
 				n = new Note(key + 2, CROTCHET);
-			else if (name.charAt(i) == 'd') 
+				break;
+			case 'd':
 				n = new Note(key + 3, CROTCHET);
-			else if (name.charAt(i) == 'e') 
+				break;
+			case 'e':
 				n = new Note(key + 4, CROTCHET);
-			else if (name.charAt(i) == 'f') 
+				break;
+			case 'f':
 				n = new Note(key + 5, CROTCHET);
-			else if (name.charAt(i) == 'g') 
+				break;
+			case 'g':
 				n = new Note(key + 6, CROTCHET);
-			else if (name.charAt(i) == 'h') 
-				n = new Note(key + 7, CROTCHET);
-			else 
-				n = new Note(key, CROTCHET);
-			
-			melody.addNote(n);	
+				break;
+			case 'h':
+				n = new Note(key, EIGHTH_NOTE);
+				break;
+			case 'i':
+				n = new Note(key + 1, EIGHTH_NOTE);
+				break;
+			case 'j':
+				n = new Note(key + 2, EIGHTH_NOTE);
+				break;
+			case 'k':
+				n = new Note(key + 3, EIGHTH_NOTE);
+				break;
+			case 'l':
+				n = new Note(key + 4, EIGHTH_NOTE);
+				break;
+			case 'm':
+				n = new Note(key + 5, EIGHTH_NOTE);
+				break;
+			case 'n':
+				n = new Note(key + 6, EIGHTH_NOTE);
+				break;
+			case 'o':
+				n = new Note(key, SIXTEENTH_NOTE);
+				break;
+			case 'p':
+				n = new Note(key + 1, SIXTEENTH_NOTE);
+				break;
+			case 'q':
+				n = new Note(key + 2, SIXTEENTH_NOTE);
+				break;
+			case 'r':
+				n = new Note(key + 3, SIXTEENTH_NOTE);
+				break;
+			case 's':
+				n = new Note(key + 4, SIXTEENTH_NOTE);
+				break;
+			case 't':
+				n = new Note(key + 5, SIXTEENTH_NOTE);
+				break;
+			case 'u':
+				n = new Note(key + 6, SIXTEENTH_NOTE);
+				break;
+			case 'v':
+				n = new Note(key, EIGHTH_NOTE_TRIPLET);
+				break;
+			case 'w':
+				n = new Note(key + 1, EIGHTH_NOTE_TRIPLET);
+				break;
+			case 'x':
+				n = new Note(key + 2, EIGHTH_NOTE_TRIPLET);
+				break;
+			case 'y':
+				n = new Note(key + 3, EIGHTH_NOTE_TRIPLET);
+				break;
+			case 'z':
+				n = new Note(key + 4, EIGHTH_NOTE_TRIPLET);
+				break;
+			case ' ':
+				n = new Note(key + 5, EIGHTH_NOTE_TRIPLET);
+				break;
+			default:
+				n = new Note(key + 6, EIGHTH_NOTE_TRIPLET);
+			}
+			System.out.println(n);
+			melody.addNote(n);
 		}
-				
+		}
 		return melody;
 	}
 }
